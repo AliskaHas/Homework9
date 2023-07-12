6b7175c372dde2b8bfca1ea658ec01221c2a2c6e
AliskaHas <nhasanova@mail.ru>
2023-07-13 02:20:02 +0300

    Сумма чисел между M и N

---
+++ Task66/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Summa(int M, int N)
+{
+    int start = M;
+    int end = N;
+    
+    if(M>N)
+    {
+        start = N;
+        end = M;
+    }
+    
+    return (end+start)*(end - start +1)/2;
+}
+
+
+
+
+//--------------основной код--------------
+
+
+int M = ReadInt("Введите первое число: ");
+int N = ReadInt("Введите второе число: ");
+
+Summa(M,N);
+System.Console.WriteLine(Summa(M,N));
+
